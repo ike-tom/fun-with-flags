dc978fa65f9ac21fe1e0d6f4d0afd4fe4361f329
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-12-22 22:35:35 +0100

    feat: create SearchInput component

---
+++ components/SearchInput/SearchInput.tsx
@@ -0,0 +1,14 @@
+
+import styles from "./SearchInput.module.scss";
+
+function SearchInput() {
+  return (
+    <form className={styles.form}>
+    <input className={styles.input} placeholder="Search for a country..."></input>
+    </form>
+      
+    
+  );
+}
+
+export default SearchInput;
