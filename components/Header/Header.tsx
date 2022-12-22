b06c2efb6c91b9b7c24e9b8b14cde38a5830658a
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-12-22 22:35:20 +0100

    feat: create Header component

---
+++ components/Header/Header.tsx
@@ -0,0 +1,14 @@
+
+import styles from "./Header.module.scss";
+
+function Header() {
+  return (
+    
+      <header className={styles.header__wrapper}>
+        <h1 className={styles.header}>Where in the world?</h1>
+      </header>
+    
+  );
+}
+
+export default Header;
