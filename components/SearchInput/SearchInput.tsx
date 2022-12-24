e0bf470143c9a8faa8ce988e28ec209d12a723d5
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-12-24 11:00:20 +0100

    refactor: refactor SearchInput component

--- components/SearchInput/SearchInput.tsx
+++ components/SearchInput/SearchInput.tsx
@@ -1,13 +1,13 @@
-
 import styles from "./SearchInput.module.scss";
 
 function SearchInput() {
   return (
     <form className={styles.form}>
-    <input className={styles.input} placeholder="Search for a country..."></input>
+      <input
+        className={styles.input}
+        placeholder="Search for a country..."
+      ></input>
     </form>
-      
-    
   );
 }
 
