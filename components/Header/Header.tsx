9804aa5747da35116d84ea3c629ac756a9dcf982
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-12-24 10:59:51 +0100

    refactor: refactor Header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,13 +1,16 @@
-
 import styles from "./Header.module.scss";
 
 function Header() {
   return (
-    
-      <header className={styles.header__wrapper}>
-        <h1 className={styles.header}>Where in the world?</h1>
-      </header>
-    
+    <header className={styles.header__wrapper}>
+      <h1 className={styles.header}>Fun with Flags</h1>
+      <div className={styles.toggleSwitch}>
+        <label className={styles.label}>
+          <input className={styles.input} type="checkbox" />
+          <span className={styles.slider}></span>
+        </label>
+      </div>
+    </header>
   );
 }
 
