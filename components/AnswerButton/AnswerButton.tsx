24a8cd6036bc790a597196d37818def0d5840db5
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-12-27 17:17:52 +0100

    feat: create AnswerButton component

--- components/AnswerButton/AnswerButton.tsx
+++ components/AnswerButton/AnswerButton.tsx
@@ -2,14 +2,18 @@ import styles from "./AnswerButton.module.scss";
 
 interface AnswerButtonProps {
   name: string;
-
   onClick?: () => void;
 }
 
 function AnswerButton(props: AnswerButtonProps) {
   const { name, onClick } = props;
   return (
-    <button className={styles.answerButton} onClick={onClick}>
+    <button
+      className={`${styles.answerButton} ${
+        name === "yes" ? styles.yesButton : styles.noButton
+      }`}
+      onClick={onClick}
+    >
       {name}
     </button>
   );
