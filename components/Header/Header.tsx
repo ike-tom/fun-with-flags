7745b807ffe003fcec6cb95ec06d4280ae3b915e
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-12-27 17:18:29 +0100

    fix: modify Header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,14 +2,18 @@ import styles from "./Header.module.scss";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../redux/store";
 import { change } from "../../redux/theme";
+import { useRouter } from "next/router";
 
 function Header() {
   const theme = useSelector((state: RootState) => state.theme.value);
   const dispatch = useDispatch();
+  const router = useRouter();
 
   return (
     <header className={styles.header__wrapper}>
-      <h1 className={styles.header}>Fun with Flags</h1>
+      <h1 className={styles.header} onClick={() => router.push("/")}>
+        Fun with Flags
+      </h1>
       <div className={styles.toggleSwitch}>
         <label className={styles.label}>
           <input
